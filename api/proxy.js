eeba14cfbc1656c320bf3c3a51d267ec6485e446
TwelthDd <79723260+twelthDd@users.noreply.github.com>
2025-03-10 14:43:15 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.sparrowslockpicks.ca";//your website url
+  let target = "https://www.sparrowslockpicks.ca/product_p/tux1.htm";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
